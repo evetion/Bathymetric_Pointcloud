41deabbf55b16dbb6fbaafd90fd5b98895b34c39
VasilisAle <42702373+VasilisAle@users.noreply.github.com>
2019-07-14 22:41:43 +0200

    Update Refraction_GreenLidar.jl

--- Refraction_GreenLidar.jl
+++ Refraction_GreenLidar.jl
@@ -4,9 +4,13 @@
 #Pkg.add(PackageSpec(url="https://github.com/evetion/LazIO.jl"))
 #Pkg.add(PackageSpec(url="https://github.com/evetion/LASindex.jl"))
 #Pkg.add(PackageSpec(url="https://github.com/JuliaGeo/Shapefile.jl"))
+#Pkg.add("AlgebraicMultigrid")#master
+#Pkg.add(PackageSpec(url="https://github.com/visr/LibLAS.jl"))
 # how to use arrays: https://en.wikibooks.org/wiki/Introducing_Julia/Arrays_and_tuples
 #using JuliaDB
 #using LASindex
+using LibLAS
+using AlgebraicMultigrid
 using Shapefile
 using LazIO
 using FileIO, LasIO
@@ -17,26 +21,31 @@ filesh = "All_Watersurfaces.shp"
 pathshape = shape*filesh
 #point cloud
 path = "C:/Users/user/Desktop/Thesis/Data/Archive/PF_0/"
-filen = "180416_110851_Scanner_1_0_RDNAP_PF0_filteredHeight.laz"
+filen = "180416_110851_Scanner_1_0_RDNAP_PF0_filteredHeight_CroppedWater.laz"
+lazinput = path*filen
+#I read directly the cropped pointcloud based on the water polygons!
+#Load a LAZ point cloud from LasIO
+testfile = File{format"LAZ_"}(lazinput)
+H1,P1 = load(testfile)
 
-#how to load LAS files
-#header, points = load(path*"LAS.las")
+#Load a LAZ point cloud with LAZio
+header, points = LazIO.load(lazinput)
 
-#Load a LAZ point cloud
-header, points = LazIO.load(path*filen)
 #Open the LAZ point cloud
-dataset = LazIO.open(path*filen)
-
+dataset = LazIO.open(lazinput)
+k = dataset.header
+k1 = dataset.point
 #Iterate over points and Store real coordinates in a table using offset and scale factor
 offset = [dataset.header.x_offset, dataset.header.y_offset, dataset.header.z_offset]
 scale_factor = [dataset.header.x_scale_factor, dataset.header.y_scale_factor, dataset.header.z_scale_factor]
 
 #Coordinates
 coords = Array{Float64}[]
+
 for p in dataset
-    x = p.X * scale_factor[1] + offset[1]
-    y = p.Y * scale_factor[2] + offset[2]
-    z = p.Z * scale_factor[3] + offset[3]
+    x = (p.X * scale_factor[1] + offset[1])
+    y = (p.Y * scale_factor[2] + offset[2])
+    z = (p.Z * scale_factor[3] + offset[3])
     push!(coords,[x, y, z, p.intensity, p.scan_angle_rank,p.classification])
 end
 println("The first element using offset: ",coords[1])
@@ -58,11 +67,12 @@ for p in coords
     end
 end
 
-#open the shapefile
+#open the shapefile with the water polygons
 handle = open(pathshape, "r") do io
     read(io, Shapefile.Handle)
 end
-#Polygons
+#Polygons:
+#to do: create an objectid for every polygon (enumerate)
 polygons = handle.shapes
 #polygon coordinates
 polygons_coordinates = Array[Array[]]
@@ -74,4 +84,73 @@ for poly in polygons
     push!(polygons_coordinates,poly_coordinates)
 end
 
-#todo-> find points inside each polygon. do it for the first water polygon
+#find centroid of the lAZ pointcloud
+function centroid(coords)
+    x_sum = 0.0
+    y_sum = 0.0
+    z_sum = 0.0
+    n= length(coords)
+    for point in coords
+        x_sum += point[1]
+        y_sum += point[2]
+        z_sum += point[3]
+    end
+    x_avg = x_sum/n
+    y_avg = y_sum/n
+    z_avg = z_sum/n
+
+    return x_avg, y_avg, z_avg
+end
+x_avg, y_avg, z_avg = centroid(coords)
+
+# How to write a LAZ pointcloud
+#define output file
+laz_out = joinpath(path, "myoutputv7.laz")
+# open reader
+reader = Ref{Ptr{Cvoid}}(C_NULL)
+LazIO.@check reader[] LazIO.laszip_create(reader)
+LazIO.@check reader[] LazIO.laszip_open_reader(reader[], lazinput, Ref(Cint(0)))
+
+# create writer
+writer = Ref{Ptr{Cvoid}}(C_NULL)
+LazIO.@check writer[] LazIO.laszip_create(writer)
+
+# copy header from reader to writer (to be updated later)
+header_ptr = Ref{Ptr{LazIO.LazHeader}}(C_NULL)
+LazIO.@check header_ptr[] LazIO.laszip_get_header_pointer(reader[], header_ptr)
+LazIO.@check writer[] LazIO.laszip_set_header(writer[], header_ptr[])
+
+#open writer
+LazIO.@check writer[] LazIO.laszip_open_writer(writer[], laz_out, Cint(1))
+
+# save the first 10 points
+len = 10
+for i = 1:len
+    #define pointer
+    point_ptr = Ref{Ptr{LazIO.LazPoint}}(C_NULL)
+    LazIO.@check point_ptr[] LazIO.laszip_get_point_pointer(reader[], point_ptr)
+    LazIO.@check reader[] LazIO.laszip_read_point(reader[])
+    #retrieve the point values
+    p = unsafe_load(point_ptr[])
+
+    # write it to the writer
+    LazIO.@check writer[] LazIO.laszip_set_point(writer[],Ref(p))
+    LazIO.@check writer[] LazIO.laszip_write_point(writer[])
+    # update the inventory
+    LazIO.@check writer[] LazIO.laszip_update_inventory(writer[])
+end
+
+# close files and destroy pointers
+LazIO.@check reader[] LazIO.laszip_close_reader(reader[])
+LazIO.@check writer[] LazIO.laszip_close_writer(writer[])
+LazIO.@check reader[] LazIO.laszip_destroy(reader[])
+LazIO.@check writer[] LazIO.laszip_destroy(writer[])
+
+#open again myoutput
+ds = LazIO.open(laz_out)
+println(ds.header.number_of_point_records)
+
+#enumerate for the pointcloud
+#for (index, value) in enumerate(dataset)
+    #println("$index $value")
+#end
